b0a4fafdcf667ded629792b0c13ffcd866fbbef8
Drone Bassan <dronebassan@gmail.com>
2025-06-02 11:23:25 -0400

    Sort by Date

--- employee.py
+++ employee.py
@@ -11,3 +11,13 @@ print(file.head())
 
 average_salary = file['Salary'].mean()
 print("Average Salary: ", average_salary)
+
+# Convert 'Join Date' column to datetime format
+file['Join Date'] = pd.to_datetime(file['Join Date'])
+
+# Sort the DataFrame by 'Join Date'
+sorted_by_date = file.sort_values(by = 'Join Date')
+
+#Print the sorted DataFrame
+print("\nEmployees sorted by Join Date:")
+print(sorted_by_date[['Employee Name', 'Join Date']])
